0ce0a2bcdc562c3df515b41c9331945adf371721
hongzhan <dbt4516@gmail.com>
2016-08-21 00:33:19 +0800

    init

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace TsUtil {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() { 
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tb = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.poweredByToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.qOSSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hongzhanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.githubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dbt4516tsUtilgitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tb
+            // 
+            this.tb.BackColor = System.Drawing.Color.White;
+            this.tb.Location = new System.Drawing.Point(0, 0);
+            this.tb.Margin = new System.Windows.Forms.Padding(0);
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(120, 21);
+            this.tb.TabIndex = 0;
+            this.tb.Text = "Welcome";
+            this.tb.Click += new System.EventHandler(this.tb_Click);
+            this.tb.KeyDown += new System.Windows.Forms.KeyEventHandler(this.enterKeyDown);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.Fuchsia;
+            this.panel1.Controls.Add(this.tb);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(167, 60);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseEnter += new System.EventHandler(this.enterForm);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "tsUTIL";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.clickTray);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.poweredByToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(146, 48);
+            // 
+            // poweredByToolStripMenuItem
+            // 
+            this.poweredByToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.qOSSToolStripMenuItem,
+            this.hongzhanToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.githubToolStripMenuItem});
+            this.poweredByToolStripMenuItem.Name = "poweredByToolStripMenuItem";
+            this.poweredByToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.poweredByToolStripMenuItem.Text = "Powered by";
+            // 
+            // qOSSToolStripMenuItem
+            // 
+            this.qOSSToolStripMenuItem.Name = "qOSSToolStripMenuItem";
+            this.qOSSToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.qOSSToolStripMenuItem.Text = "QOSS";
+            // 
+            // hongzhanToolStripMenuItem
+            // 
+            this.hongzhanToolStripMenuItem.Name = "hongzhanToolStripMenuItem";
+            this.hongzhanToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.hongzhanToolStripMenuItem.Text = "zyl whz";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(142, 22);
+            this.toolStripMenuItem2.Text = "2016-08-21";
+            // 
+            // githubToolStripMenuItem
+            // 
+            this.githubToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dbt4516tsUtilgitToolStripMenuItem});
+            this.githubToolStripMenuItem.Name = "githubToolStripMenuItem";
+            this.githubToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.githubToolStripMenuItem.Text = "github";
+            // 
+            // dbt4516tsUtilgitToolStripMenuItem
+            // 
+            this.dbt4516tsUtilgitToolStripMenuItem.Name = "dbt4516tsUtilgitToolStripMenuItem";
+            this.dbt4516tsUtilgitToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.dbt4516tsUtilgitToolStripMenuItem.Text = "~/dbt4516/tsUtil.git";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.exitToolStripMenuItem.Text = "exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exit);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(120, 83);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Deactivate += new System.EventHandler(this.lostFocus);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem poweredByToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem qOSSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hongzhanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem githubToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dbt4516tsUtilgitToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TsUtil {
+    public partial class Form1 : Form {
+
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+
+
+        private static int WM_CLIPBOARDUPDATE = 0x031D;
+        private static int ns;
+
+
+
+        public Form1() {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None; 
+
+
+            CheckForIllegalCrossThreadCalls = false;
+            AddClipboardFormatListener(this.Handle);
+            //RegisterHotKey(this.Handle, this.GetType().GetHashCode(), 0x0008, (int)'T');
+            hide();
+
+        }
+
+        public static DateTime unixTimeStampToDateTime(double unixTimeStamp) {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+
+        public static long getDateTime(String time) {
+            var now = DateTime.Now;
+            int y = now.Year, M = now.Month, d = now.Day, h = now.Hour, m = now.Minute;
+            if (time.Length >= 2) {
+                var t = time.Substring(time.Length - 2, 2);
+                m = int.Parse(t);
+            }
+            if (time.Length >= 4) {
+                var t = time.Substring(time.Length - 4, 2);
+                h = int.Parse(t);
+            }
+            if (time.Length >= 6) {
+                var t = time.Substring(time.Length - 6, 2);
+                d = int.Parse(t);
+            }
+            if (time.Length >= 8) {
+                var t = time.Substring(time.Length - 8, 2);
+                M = int.Parse(t);
+            }
+            if (time.Length >= 12) {
+                var t = time.Substring(time.Length - 12, 4);
+                y = int.Parse(t);
+            }
+            var dt = new DateTime(y, M, d, h, m, 0, 0, System.DateTimeKind.Local);
+            var start = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).AddHours(8);
+            return (dt.Ticks - start.Ticks) / 1000 / 1000 / 10;
+        }
+
+        protected override void DefWndProc(ref Message m) {
+            if (m.Msg == WM_CLIPBOARDUPDATE) {
+                var text = Clipboard.GetText();
+                if (text.Length == 13) {
+                    text = text.Substring(0, 10);
+                }
+                try {
+                    if (text.Length == 10 ) {
+                        var time = unixTimeStampToDateTime(double.Parse(text));
+                        var s = time.ToString("yyyy-MM-dd HH:mm:ss");
+                        var now = DateTime.Now;
+                        if (time.Year >= now.Year - 10 && time.Year <= now.Year + 10) {
+                            tb.Text = s;
+                            var p=System.Windows.Forms.Cursor.Position;
+                            show();
+                            hide();
+                        }
+                    }
+                } catch (Exception e) {
+
+                }
+            } else {
+                base.DefWndProc(ref m);
+            }
+        }
+
+        public void hide() {
+            System.Threading.Timer timer = null;
+            int nsI=DateTime.Now.Second;
+            Form1.ns = nsI;
+            timer = new System.Threading.Timer((obj) => {
+                if (nsI == Form1.ns) { 
+                slideRight();
+                }
+                timer.Dispose();
+            },null, 3000, System.Threading.Timeout.Infinite);
+        }
+
+        public void show() {
+            var p = System.Windows.Forms.Cursor.Position;
+            var res = Screen.PrimaryScreen.Bounds;
+            var nX = p.X - 50 > (res.Width - 120) ? res.Width - 120 : p.X - 50;
+            nX = nX < 10 ? 10 : nX;
+            var nY = (p.Y - 50) < 10 ? 10 : p.Y-50;
+            //Console.WriteLine(nX + " " + nY);
+            Location = new Point(nX, nY);
+            
+        }
+
+        public void slideRight() {
+            if (tb.Focused)
+                return; 
+            Location = new Point(0, -27);
+        }
+
+        protected override void WndProc(ref Message m) {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);
+            //if (m.Msg == 0x0312)
+            //    MessageBox.Show("Hotkey pressed");
+        }
+
+       
+
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+
+        private void tb_Click(object sender, EventArgs e) {
+            if (!String.IsNullOrEmpty(tb.Text)) {
+                tb.SelectAll();
+            }
+        }
+
+        private void enterKeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                try {
+                    var ts = getDateTime(tb.Text).ToString();
+                    if (ts.Length == 10) {
+                        Clipboard.SetText(ts);
+                    }
+                } catch (Exception exc) {
+
+                }
+
+            }
+        }
+
+        private void enterForm(object sender, EventArgs e) {
+            tb.Text = "entered";
+        }
+
+        private void clickTray(object sender, MouseEventArgs e) {
+            if (Location.Y == -27) { 
+            show();
+            Activate();
+            tb.Focus();
+            } else {
+                slideRight();
+            }
+        }
+
+        private void lostFocus(object sender, EventArgs e) { 
+            //hide();
+        }
+
+        private void exit(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void poweredByQOSSToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        
+       
+    }
+}
